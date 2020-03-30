4fe6adcf27e7447a356ed025bb3b219620fc4e9e
nick-funk <nick.funk@outlook.com>
2020-03-30 15:07:21 -0600

    Update my profile tab bar to new branding
    CORL-902

--- src/core/client/stream/tabs/Profile/Profile.tsx
+++ src/core/client/stream/tabs/Profile/Profile.tsx
@@ -75,13 +75,14 @@ const Profile: FunctionComponent<ProfileProps> = props => {
     <HorizontalGutter size="double">
       <UserBoxContainer viewer={props.viewer} settings={props.settings} />
       <TabBar
-        variant="secondary"
+        variant="streamSecondary"
         activeTab={local.profileTab}
         onTabClick={onTabClick}
         className={CLASSES.tabBarMyProfile.$root}
       >
         <Tab
           tabID="MY_COMMENTS"
+          variant="streamSecondary"
           className={cn(CLASSES.tabBarMyProfile.myComments, {
             [CLASSES.tabBarMyProfile.active]:
               local.profileTab === "MY_COMMENTS",
@@ -93,6 +94,7 @@ const Profile: FunctionComponent<ProfileProps> = props => {
         </Tab>
         <Tab
           tabID="PREFERENCES"
+          variant="streamSecondary"
           className={cn(CLASSES.tabBarMyProfile.preferences, {
             [CLASSES.tabBarMyProfile.active]:
               local.profileTab === "PREFERENCES",
@@ -105,6 +107,7 @@ const Profile: FunctionComponent<ProfileProps> = props => {
         {showAccountTab && (
           <Tab
             tabID="ACCOUNT"
+            variant="streamSecondary"
             className={cn(CLASSES.tabBarMyProfile.settings, {
               [CLASSES.tabBarMyProfile.active]: local.profileTab === "ACCOUNT",
             })}
